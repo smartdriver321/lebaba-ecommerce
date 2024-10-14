848aa16293ea62e12a6ae3be520819c448c7b7c0
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-10-14 11:37:47 +0700

    Add Footer to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/navbar'
+import Footer from './components/footer'
 
 export default function App() {
 	return (
 		<>
 			<Navbar />
 			<Outlet />
+			<Footer />
 		</>
 	)
 }
