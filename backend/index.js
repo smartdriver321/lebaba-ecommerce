6508274c06d33c8325a29e3d338c6d25ecd9fa49
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-10-14 16:10:57 +0700

    Configure Mongoose and MongoDB to server

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 const app = express()
-const port = 3000
+const PORT = 3000
 
-app.get('/', (req, res) => {
-	res.send('Hello World!')
-})
+async function main() {
+	await mongoose.connect(process.env.MONGO_URI)
 
-app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`)
+	app.get('/', (req, res) => {
+		res.send('Lebaba E-commerce Server is running....!')
+	})
+}
+
+app.listen(PORT, async () => {
+	await main()
+		.then(() => console.log('MongoDB is successfully connected.'))
+		.catch((err) => console.log(err))
+	console.log(`App is listening on port ${PORT}`)
 })
