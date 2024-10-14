17196d64b5ce1506f12c0961d99b9fe95a97719c
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-10-14 10:30:09 +0700

    Add HeroSection to Home

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -1,11 +1,13 @@
 import Banner from './banner'
 import Categories from './categories'
+import HeroSection from './hero-section'
 
 export default function Home() {
 	return (
 		<>
 			<Banner />
 			<Categories />
+			<HeroSection />
 		</>
 	)
 }
