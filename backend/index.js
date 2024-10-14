7c84e086fa2279f255445c80d7a9be4cad7398c3
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-10-14 20:44:01 +0700

    Create user API

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,27 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 require('dotenv').config()
+const authRouter = require('./src/users/user.route')
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 5000
+
+app.use(express.json({ limit: '25mb' }))
+//app.use(express.urlencoded({ limit: '25mb' }))
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(
+	cors({
+		origin: 'http://localhost:5173',
+		credentials: true,
+	})
+)
+
+app.use('/api/auth', authRouter)
 
 async function main() {
 	await mongoose.connect(process.env.MONGO_URI)
