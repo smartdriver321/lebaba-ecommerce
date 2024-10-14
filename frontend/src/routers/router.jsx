1db09cc40d66d6259432dbe6a31deee5a01d288e
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-10-14 11:51:27 +0700

    Create CategoryPage

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -2,12 +2,16 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import App from '../App'
 import Home from '../pages/home/Home'
+import CategoryPage from '../pages/category/CategoryPage'
 
 const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <App />,
-		children: [{ path: '/', element: <Home /> }],
+		children: [
+			{ path: '/', element: <Home /> },
+			{ path: '/categories/:categoryName', element: <CategoryPage /> },
+		],
 	},
 ])
 
